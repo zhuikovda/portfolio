054ec49002b7c8abdb362f680506bf482ec7b963
zhuikovda <zhuikovda@gmail.com>
2023-06-07 12:02:14 +0300

    add margin ExspirEduc

--- src/main/about/ExperEduc.tsx
+++ src/main/about/ExperEduc.tsx
@@ -1,24 +1,29 @@
-import React, { FC } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import React, { FC } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
 
 type ExperEducPropsType = {
-    // title: string
-    // img: string
-    year: string;
-    workPlace: string;
-    text: string;
+  // title: string
+  // img: string
+  year: string;
+  workPlace: string;
+  text: string;
 };
 
 export const ExperEduc: FC<ExperEducPropsType> = (props) => {
-    return (
-        <div>
-            <div>
-                <FontAwesomeIcon icon={faCalendarAlt} />
-                {props.year}
-            </div>
-            <div>{props.workPlace}</div>
-            <div>{props.text}</div>
-        </div>
-    );
+  return (
+    <Container>
+      <div>
+        <FontAwesomeIcon icon={faCalendarAlt} />
+        {props.year}
+      </div>
+      <div>{props.workPlace}</div>
+      <div>{props.text}</div>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  margin-top: 20px;
+`;
